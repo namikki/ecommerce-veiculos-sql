a4ddffb7efc5de7da4a653d7d5a36e4bdbda3ed8
Bianca da Silva Rodrigues <85198342+namikki@users.noreply.github.com>
2023-03-13 14:03:14 -0300

    comandos básicos com WHERE

---
+++ comando_where.sql
@@ -0,0 +1,46 @@
+-- PARA QUE SERVE ##################################################################
+-- Serve para filtrar linhas de acordo com uma condição
+
+
+-- SINTAXE #########################################################################
+select coluna_1, coluna_2, coluna_3
+from schema_1.tabela_1
+where condição_x=true
+
+
+-- EXEMPLOS ########################################################################
+
+-- (Exemplo 1) Filtro com condição única
+-- Liste os emails dos clientes da nossa base que moram no estado de Santa Catarina
+SELECT email, state
+FROM sales.customers
+WHERE state = 'SC'
+
+--Conferir os dados da coluna state
+SELECT DISTINCT state
+FROM sales.customers
+
+-- (Exemplo 2) Filtro com mais de uma condição
+-- Liste os emails dos clientes da nossa base que moram no estado de Santa Catarina
+-- ou Mato Grosso do Sul
+SELECT email, state
+FROM sales.customers
+WHERE state = 'SC' OR state = 'MS'
+
+-- (Exemplo 3) Filtro de condição com data
+-- Liste os emails dos clientes da nossa base que moram no estado de Santa Catarina 
+-- ou Mato Grosso do Sul e que tem mais de 30 anos
+SELECT email, state, birth_date
+FROM sales.customers
+WHERE (state = 'SC' OR state = 'MS') AND birth_date < '1993-03-13'
+
+--Conferir a data de nascimento
+SELECT DISTINCT birth_date
+FROM sales.customers
+
+-- RESUMO ##########################################################################
+-- (1) Comando utilizado para filtrar linhas de acordo com uma condição
+-- (2) No PostgreSQL são utilizadas aspas simples para delimitar strings 
+-- (3) string = sequência de caracteres = texto
+-- (4) Pode-se combinar mais de uma condição utilizando os operadores lógicos
+-- (5) No PostgreSQL as datas são escritas no formato 'YYYY-MM-DD' ou 'YYYYMMDD'
